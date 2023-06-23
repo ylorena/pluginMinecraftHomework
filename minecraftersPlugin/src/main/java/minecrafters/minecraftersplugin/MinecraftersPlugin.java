fe1417b69136007b69e029f91894b795bb1d17b5
TheAndreLuiz <andreluizgaspar1@gmail.com>
2023-06-23 17:22:26 -0300

    minor changes, update read me, fix level bar

--- minecraftersPlugin/src/main/java/minecrafters/minecraftersplugin/MinecraftersPlugin.java
+++ minecraftersPlugin/src/main/java/minecrafters/minecraftersplugin/MinecraftersPlugin.java
@@ -1,17 +1,14 @@
 package minecrafters.minecraftersplugin;
-
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MinecraftersPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         getServer().getPluginManager().registerEvents(new MiningListener(this), this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
--- minecraftersPlugin/src/main/java/minecrafters/minecraftersplugin/PlayerLevelManager.java
+++ minecraftersPlugin/src/main/java/minecrafters/minecraftersplugin/PlayerLevelManager.java
@@ -6,7 +6,6 @@ import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-
 public class PlayerLevelManager {
     private int level;
     private int experience;
@@ -31,7 +30,6 @@ public class PlayerLevelManager {
         experience += amount;
         bossBar.setProgress((double) experience / getExperienceRequiredForLevelUp(level));
 
-        // Check if the player has enough experience to level up
         if (experience >= getExperienceRequiredForLevelUp(level)) {
             levelUp();
         }
@@ -40,12 +38,9 @@ public class PlayerLevelManager {
     private void levelUp() {
         level++;
         bossBar.setTitle("Level: " + level);
-        // You can perform additional actions when the player levels up
     }
 
     private int getExperienceRequiredForLevelUp(int currentLevel) {
-        // Define a formula to calculate the experience required for level up
-        // This can be a linear or exponential formula based on your design
         return currentLevel * 10; // Example formula: 10 experience points per level
     }
 }
