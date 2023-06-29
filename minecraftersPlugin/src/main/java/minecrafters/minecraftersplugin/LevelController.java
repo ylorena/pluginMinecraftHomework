9e52424a3bb70dc8ca8eebbdb87809ab47b54674
TheAndreLuiz <andreluizgaspar1@gmail.com>
2023-06-29 17:46:13 -0300

    update to generic listener

---
+++ minecraftersPlugin/src/main/java/minecrafters/minecraftersplugin/LevelController.java
@@ -0,0 +1,103 @@
+package minecrafters.minecraftersplugin;
+
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
+import org.bukkit.boss.KeyedBossBar;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
+
+public class LevelController {
+    private int level = 1;
+    private int experience = 0;
+    private BossBar bossBar = null;
+    private String tipo;
+    static private final UUID playerId = null;
+    static private final Plugin plugin = null;
+    static private LevelController levelController = null;
+
+        // this.tipo = tipo;
+        // this.plugin = plugin;
+        // this.playerId = player.getUniqueId();
+
+        // this.bossBar = plugin.getServer().createBossBar("", BarColor.YELLOW,
+        //         BarStyle.SEGMENTED_6); // TODO segments, color and style
+        // bossBar.addPlayer(player);
+
+        // loadPlayerData();
+        // updateBossBar();
+
+    static public LevelController getLevelController() {
+        if (levelController == null) {
+            levelController = new LevelController();
+        }
+        return levelController;
+    }
+
+    private void levelUp() {
+        level++;
+        savePlayerData();
+        updateBossBar();
+    }
+
+    public void addExperience(int amount) {
+        experience += amount;
+
+        while (experience >= getExperienceRequiredForLevelUp(level)) {
+            levelUp();
+            experience -= getExperienceRequiredForLevelUp(level - 1);
+            savePlayerData();
+        }
+
+        updateBossBar();
+        savePlayerData();
+    }
+
+    private int getExperienceRequiredForLevelUp(int currentLevel) {
+        return (int) Math.round(100 * Math.pow(1.1, currentLevel));
+    }
+
+    private void updateBossBar() {
+        double progress = (double) experience / getExperienceRequiredForLevelUp(level);
+        bossBar.setProgress(progress);
+        bossBar.setTitle(this.tipo + " - Level " + level);
+    }
+
+    private void loadPlayerData() {
+        File dataFile = new File("plugins/MinecraftersPlugin/playerdata/" + playerId + ".dat");
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(dataFile))) {
+            Map<String, String> loadedData = (Map<String, String>) ois.readObject();
+
+            this.level = Integer.parseInt(loadedData.get("tipo").split("+")[0]);
+            this.experience = Integer.parseInt(loadedData.get("tipo").split("+")[1]);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void savePlayerData() {
+        // Save player's data to file
+        File dataFolder = new File(plugin.getDataFolder(), "playerdata");
+        if (!dataFolder.exists()) {
+            dataFolder.mkdirs();
+        }
+
+        File dataFile = new File(dataFolder, playerId + ".dat");
+        Map<String, String> data = new HashMap<>();
+
+        data.put(tipo, "{0}+{1}".format(Integer.toString(this.level), Integer.toString(this.experience)));
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(dataFile))) {
+            oos.writeObject(data);
+            plugin.getServer().getConsoleSender().sendMessage("[MinecraftersPlugin] Player data saved for " + playerId);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- minecraftersPlugin/src/main/java/minecrafters/minecraftersplugin/MinecraftersPlugin.java
+++ minecraftersPlugin/src/main/java/minecrafters/minecraftersplugin/MinecraftersPlugin.java
@@ -1,16 +1,42 @@
 package minecrafters.minecraftersplugin;
-import minecrafters.minecraftersplugin.listeners.EntityListener;
-import minecrafters.minecraftersplugin.listeners.MiningListener;
+
+import minecrafters.minecraftersplugin.listeners.GenericListener;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.EventException;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.EventExecutor;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MinecraftersPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new MiningListener(this), this);
-        getServer().getPluginManager().registerEvents(new EntityListener(this), this);
+        registerGenericEventHandler(BlockBreakEvent.class, this, new GenericListener());
+        registerGenericEventHandler(PlayerJoinEvent.class, this, new GenericListener());
     }
+
     @Override
     public void onDisable() {
+    }
+
+    public <T extends Event> void registerGenericEventHandler(Class<T> eventClass, Plugin plugin, Listener listener) {
+        EventExecutor eventExecutor = new EventExecutor() {
+            @Override
+            public void execute(Listener listener, Event event) throws EventException {
+                System.out.println(event.getEventName());
+            }
+        };
+
+        plugin.getServer().getPluginManager().registerEvent(
+                eventClass,
+                listener,
+                EventPriority.NORMAL,
+                eventExecutor,
+                plugin);
     }
 }
