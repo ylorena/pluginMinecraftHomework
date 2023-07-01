9a26b650a6b6cbd747dd168bf00d22ef91a286f8
TheAndreLuiz <andreluizgaspar1@gmail.com>
2023-07-01 16:52:49 -0300

    minor changes

--- minecraftersPlugin/src/main/java/minecrafters/minecraftersplugin/EventToFunction.java
+++ minecraftersPlugin/src/main/java/minecrafters/minecraftersplugin/EventToFunction.java
@@ -1,16 +1,10 @@
 package minecrafters.minecraftersplugin;
 
-import org.bukkit.Bukkit;
-
 public class EventToFunction {
-    public void test() {
-        Bukkit.getServer().broadcastMessage("Test");
-    }
-
     private IEventToFunction[] eventToFunctions = new IEventToFunction[] {
             new IEventToFunction() {
                 public void call() {
-                    test();
+                    LevelController.getLevelController().updateBossBar();
                 }
             },
     };
